6d92ffe8b2b1bde393e43ca3d0bf5eea5f393e8a
Emilykradke <emilykradke@gmail.com>
2019-05-28 14:39:26 -0500

    updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,7 @@ class Home extends Component {
         <div className="home-container">
           <h1 className="heading-l">Hello, I'm <span className="loud">Emily Radke</span>.</h1>
           <h2 className="heading-l">I'm a full-stack web developer.</h2>
-          {/* <p className="paragraph">This is description 1.</p>
-          <p className="paragraph">This is description 2.</p>
-          <p className="paragraph">This is description 3.</p> */}
+          {/* <p className="paragraph">This is description 1.</p> */}
         </div>
       </section>
     );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,31 +3,29 @@ import Scrollspy from 'react-scrollspy';
 
 import Hamburger from './Hamburger';
 
-const navItems = [{id: 'about', text: 'About'}, 
-                  {id: 'portfolio', text: 'Portfolio'}, 
-                  {id: 'contact', text: 'Contact'}];
+// const navItems = [{id: 'about', text: 'About'}, 
+//                   {id: 'portfolio', text: 'Portfolio'}, 
+//                   {id: 'contact', text: 'Contact'}];
 
 
 class Nav extends Component {
 
   render() {
-    const { toggleMenu } = this.props;
     return (
       <nav>
         <Scrollspy className="nav-list" 
-          items={ ['about', 'portfolio', 'contact'] } currentClassName="active" offset={ -200 }>
-          {navItems.map((item, i) => {
-            return (
-              <li key={i}>
-                <a href={`#${item.id}`}>{item.text}</a>
-              </li>
-            );
-          })}
+          items={ ['about', 'portfolio', 'contact'] } offset={ -200 }>
+          <li>
+              <a href={`#about`}>About</a>
+              <a href={`#portfolio`}>Portfolio</a>
+              <a href={`#contact`}>Contact</a>
+              <a href={`https://www.dropbox.com/s/xr6kiy518oho4jh/Resume.pdf?dl=0`}>Resume</a>
+            </li>
         </Scrollspy>
 
         <Hamburger 
           menuOpen={this.props.menuOpen}
-          toggleMenu={toggleMenu}
+          toggleMenu={this.props.toggleMenu}
         />
       </nav>
     );
